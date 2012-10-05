333ddfd8d4dee2f8d872d68797f317a8375a4f6f
Fabio Napoleoni <f.napoleoni@gmail.com>
2012-10-05 18:13:50 +0200

    Fixed message for have_logstash_field matcher

--- spec/support/have_logstash_matcher.rb
+++ spec/support/have_logstash_matcher.rb
@@ -4,6 +4,8 @@ RSpec::Matchers.define :have_logstash_field do |field|
   end
 
   match do |grok_match|
+    # avoid running the assertion on non matching data
+    grok_match.should be_a Grok::Match
     grok_captures = grok_match.captures()
     grok_field = grok_captures.keys.grep(/.*:#{field}$/)
     if @value
@@ -14,7 +16,9 @@ RSpec::Matchers.define :have_logstash_field do |field|
   end
 
   failure_message_for_should do |grok_match|
-    if @value
+    if !grok_match.is_a?(Grok::Match) # non matching data case
+      "grok did not matched the text for the given argument"
+    elsif @value
       "\"#{field}\" isn't set to \"#{@value}\" in #{grok_match.captures()}"
     else
       "\"#{field}\" suffix not present in #{grok_match.captures().keys}"
